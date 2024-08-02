81df0583d2676af0f17b4a28c2a6ffe505d17d56
haru <harutsuka0427@gmail.com>
2024-08-02 18:57:51 +0900

    add cloudinary api key

--- app.rb
+++ app.rb
@@ -5,6 +5,16 @@ require 'sinatra/reloader' if development?
 require 'sinatra/activerecord'
 require './models'
 
+before do 
+    Dotenv.load
+    Cloudinary.config do |config|
+        config.cloud_name = ENV['CLOUD_NAME']
+        config.api_key  = ENV['CLOUDINARY_API_KEY']
+        config.api_secret = ENV['CLOUDINARY_API_SECRET']
+        config.secure  = true
+    end 
+end
+
 get "/" do
     erb :index
 end
