6f2a3ce27966d758715e837485efe8cc653869ce
haru <harutsuka0427@gmail.com>
2024-09-06 18:33:37 +0900

    Update models

--- models.rb
+++ models.rb
@@ -4,10 +4,14 @@ Bundler.require
 ActiveRecord::Base.establish_connection
 
 class Post < ActiveRecord::Base
+  belongs_to :category
+  has_many :image
 end
 
 class Category < ActiveRecord::Base
+  has_many :post
 end
 
 class Image < ActiveRecord::Base
+  belongs_to :post
 end
